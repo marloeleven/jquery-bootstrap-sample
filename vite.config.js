3803a5a218e0092baca70835fd6bcd6e66df62e7
Marlo Dela Torre <marlodelatorre11@gmail.com>
2023-09-11 20:50:21 +0800

    fix assets path

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
+import { defineConfig } from 'vite'
+
 const path = require('path')
 
-export default {
+export default defineConfig({
   root: path.resolve(__dirname, 'src'),
+  base: './',
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     }
   },
   build: {
-    outDir: '../docs'
+    outDir: '../docs',
   },
   server: {
     port: 8080,
     hot: true
   }
-}
+})
